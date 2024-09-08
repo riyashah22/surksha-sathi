be6df77d320dfa99fa5f52440f44a68f61d3dbdb
Riya Shah <riyapshah22@gmail.com>
2024-09-08 14:46:27 +0530

    fefe

---
+++ lib/screens/health/health_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HealthScreen extends StatelessWidget {
+  const HealthScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
